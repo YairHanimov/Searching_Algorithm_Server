dd554d74637a8babff5824aafa19b5363bdcffd5
yair <hthtr0@gmail.com>
2020-01-12 20:13:29 +0200

    client server.timeout.

--- MyTestClientHandler.cpp
+++ MyTestClientHandler.cpp
@@ -2,8 +2,16 @@
 // Created by eyal on 12.1.2020.
 //
 
+#include <stdio.h>
 #include "MyTestClientHandler.h"
-
+#include <sys/socket.h>
+#include "MySerialServer.h"
+#include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include "chrono"
 void MyTestClientHandler::handleClient(int inputStream, int outputStream) {
+    char buffer[1024] = {0};
+    ssize_t valread = read(inputStream, buffer, 1024);
 
 }
---
+++ StringReverser.h
@@ -0,0 +1,19 @@
+//
+// Created by yair on 12/01/2020.
+//
+
+#ifndef SEARCHING_ALGORITHM_SERVER_STRINGREVERSER_H
+#define SEARCHING_ALGORITHM_SERVER_STRINGREVERSER_H
+#include "string"
+using namespace std;
+
+class StringReverser {
+private: string mystring;
+public: StringReverser(string s){
+    this->mystring=s;
+}
+
+};
+
+
+#endif //SEARCHING_ALGORITHM_SERVER_STRINGREVERSER_H
