274ca0f72170f03d3d7b993ce481c89627123e16
eyalgolan <eyalgolan96@gmail.com>
2020-01-12 15:09:56 +0200

    working on general classes structure

---
+++ Solver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by eyal on 12.1.2020.
+//
+
+#include "Solver.h"
---
+++ Solver.h
@@ -0,0 +1,12 @@
+//
+// Created by eyal on 12.1.2020.
+//
+
+#ifndef SEARCHING_ALGORITHM_SERVER_SOLVER_H
+#define SEARCHING_ALGORITHM_SERVER_SOLVER_H
+
+class Solver {
+
+};
+
+#endif //SEARCHING_ALGORITHM_SERVER_SOLVER_H
