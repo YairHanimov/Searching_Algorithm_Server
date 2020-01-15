382cd65b201afc597b4a7cd0e46d1c1ca5af18a6
eyalgolan <eyalgolan96@gmail.com>
2020-01-16 00:58:35 +0200

    Revert "changing to work by ex uml1"
    This reverts commit 3308deeeeaf3e839214e21876a87fc180e5ecbac.

--- FileCacheManager.h
+++ FileCacheManager.h
@@ -18,55 +18,52 @@ public:
     unsigned long capacity = 0;
 
     void insert(problem p, solution s) override {
-//        ofstream in_file;
-//        string filename = p;
-//        in_file.open(filename, ios::binary);
-//        if (!in_file) {
-//            throw "error open file";
-//        }
-//        in_file.write((char *) &s, sizeof(s));
-//        in_file.close();
-//        auto it = cache.find(p);
-//        dq.push_front(pair<problem, solution>(p, s));
-//        if (it != cache.end()) {
-//            dq.erase(it->second);
-//            cache.erase(it);
-//        }
-//        cache[p] = dq.begin();
-//
-//        if (cache.size() > capacity) {
-//            auto last = dq.end();
-//            last--;
-//            cache.erase(last->first);
-//            dq.pop_back();
-//        }
+        ofstream in_file;
+        string filename = p;
+        in_file.open(filename, ios::binary);
+        if (!in_file) {
+            throw "error open file";
+        }
+        in_file.write((char *) &s, sizeof(s));
+        in_file.close();
+        auto it = cache.find(p);
+        dq.push_front(pair<problem, solution>(p, s));
+        if (it != cache.end()) {
+            dq.erase(it->second);
+            cache.erase(it);
+        }
+        cache[p] = dq.begin();
+
+        if (cache.size() > capacity) {
+            auto last = dq.end();
+            last--;
+            cache.erase(last->first);
+            dq.pop_back();
+        }
     }
 
     solution* get(problem p) override {
-//        solution *s;
-//        auto it = cache.find(p);
-//        if (it == cache.end()) {
-//            // if (cache.find(p)==cache.end){
-//            fstream in_file;
-//            string filename = p;
-//            in_file.open(filename);
-//            if (!in_file) {
-//                return nullptr;
-//            }
-//            in_file.read((char *) &s, sizeof(s));
-////            insert(p, s);
-//            in_file.close();
-//            return s;
-//
-//        } else {
-//
-//            dq.splice(dq.begin(), dq, it->second);
-//            insert(p, it->second->second);
-//            return &it->second->second;
-//
-//        }
-    solution *s;
-    return  s;
+        solution *s;
+        auto it = cache.find(p);
+        if (it == cache.end()) {
+            // if (cache.find(p)==cache.end){
+            fstream in_file;
+            string filename = p;
+            in_file.open(filename);
+            if (!in_file) {
+                return nullptr;
+            }
+            in_file.read((char *) &s, sizeof(s));
+//            insert(p, s);
+            in_file.close();
+            return s;
+
+        } else {
+
+            dq.splice(dq.begin(), dq, it->second);
+            insert(p, it->second->second);
+            return &it->second->second;
+        }
     }
 
     FileCacheManager() {
