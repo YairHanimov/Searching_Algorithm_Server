c4a76ed59c799aa86bc67f32f00b4648d6bd5744
eyalgolan <eyalgolan96@gmail.com>
2020-01-15 23:52:49 +0200

    changing to work by ex uml

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 #include "MyTestClientHandler.h"
 #include "StringReverser.h"
 #include "FileCacheManager.h"
-
 using namespace std;
 
 int main() {
-    auto *ch = new MyTestClientHandler();
     auto *cm = new FileCacheManager<string,string>();
-    MySerialServer<string,string> *serial_server = new MySerialServer<string,string>(5600, ch, cm);
+    auto *ch = new MyTestClientHandler<string,string>(cm);
+    MySerialServer<string,string> *serial_server = new MySerialServer<string,string>(5600, ch);
     //  a->open(5600, (MyTestClientHandler)*ch);
     string strtest = "hello world";
 
