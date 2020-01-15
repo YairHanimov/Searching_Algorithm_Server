3308deeeeaf3e839214e21876a87fc180e5ecbac
eyalgolan <eyalgolan96@gmail.com>
2020-01-16 00:57:45 +0200

    changing to work by ex uml1

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main() {
     auto *cm = new FileCacheManager<string,string>();
     auto *ch = new MyTestClientHandler<string,string>(cm);
-    MySerialServer<string,string> *serial_server = new MySerialServer<string,string>(5600, ch);
-    serial_server->open();
-    string strtest = "hello world";
-
-    StringReverser *rev = new StringReverser();
-
-    cout << rev->solve(strtest) << endl;
+//    MySerialServer<string,string> *serial_server = new MySerialServer<string,string>(5600, ch);
+//    serial_server->open();
+//    string strtest = "hello world";
+//
+//    StringReverser *rev = new StringReverser();
+//
+//    cout << rev->solve(strtest) << endl;
     return 0;
 }
