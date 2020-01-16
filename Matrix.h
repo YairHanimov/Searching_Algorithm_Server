9f24f1c4cb465652aca05ca032438073631dab75
eyalgolan <eyalgolan96@gmail.com>
2020-01-16 15:21:01 +0200

    fixed OA-Matrix-state build issues

--- Matrix.h
+++ Matrix.h
@@ -11,23 +11,27 @@
 
 using namespace std;
 
-class Matrix : public Searchable<Cell>{
+class Matrix : public Searchable<Cell> {
 public:
-    vector<vector<State<Cell>*>> matrix;
+    vector<vector<State<Cell> *>> matrix;
     State<Cell> *start;
     State<Cell> *end;
-    Matrix(vector<vector<State<Cell>*>> m, State<Cell> *s, State<Cell> *e) {
+
+    Matrix(vector<vector<State<Cell> *>> m, State<Cell> *s, State<Cell> *e) {
         this->matrix = m;
         this->start = s;
         this->end = e;
     }
-    State<Cell> getInitialState() override{
+
+    State<Cell> getInitialState() override {
         return *this->start;
     }
-    bool isGoalState(State<Cell> c) override{
+
+    bool isGoalState(State<Cell> c) override {
 
     }
-    virtual vector<State<Cell>> getAllPossibleStates(State<Cell>){}
+
+    virtual vector<State<Cell>> getAllPossibleStates(State<Cell>) {}
 };
 
 #endif //SEARCHING_ALGORITHM_SERVER_MATRIX_H
