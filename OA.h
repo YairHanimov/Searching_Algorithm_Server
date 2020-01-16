5635507669fd331aed50bad9991dbe9b3e080e2f
yair <hthtr0@gmail.com>
2020-01-16 22:44:57 +0200

    server cash seaarch21133

--- OA.h
+++ OA.h
@@ -11,6 +11,8 @@
 #include "Searcher.h"
 # include "Matrix.h"
 #include <unistd.h>
+#include "Cell.h"
+#include "State.h"
 
 using namespace std;
 
@@ -39,7 +41,45 @@ public:
             token = strtok(NULL, "\n");
 
         }
+        string lexer="";
+        int row=0;
+        int call=0;
+        int mysize=line.size();
+
+        while(row<(mysize-3)){
+            lexer+=line.front();
+            line.pop_back();
+            char *token = strtok(strToChar, ",");
+            while (token != NULL) {
+                Cell *c= new Cell(row,call);
+                State<Cell> *st = new State<Cell>((double )atoi(token));
+                call++;
+                token = strtok(NULL, ",");
+            }
+            row++;
+        }
+        char *token = strtok(strToChar, ",");
+        if (token != NULL) {
+            double  start1=(double )atoi(token);
+            token = strtok(NULL, ",");
+        }
+        if (token != NULL) {
+            double  start2=(double )atoi(token);
+            token = strtok(NULL, ",");
+        }
+        if (token != NULL) {
+            double  end1=(double )atoi(token);
+            token = strtok(NULL, ",");
+        }
+        if (token != NULL) {
+            double  end2=(double )atoi(token);
+            token = strtok(NULL, ",");
+        }
+        Cell *startcell=new Cell(start1,start2);
+        Cell *endedsell=new Cell(start1,start2);
+        State<Cell> *start = new State<Cell>()
     }
+
 
 private:
     Searcher<problem> oa_searcher;
