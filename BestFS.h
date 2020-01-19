c15a3fbe0e69cc4f4639318519891b40be8609e3
eyalgolan <eyalgolan96@gmail.com>
2020-01-19 15:11:05 +0200

    working on BestFS

--- BestFS.h
+++ BestFS.h
@@ -14,32 +14,33 @@ using namespace std;
 template<class T>
 class BestFS : public Searcher<T> {
 public:
-    vector<vector<T>*> search(Searchable<State<T>>* searchable) override {
-        vector<State<T>*> path;
+    vector<vector<T> *> search(Searchable<State<T>> *searchable) override {
+        vector<State<T> *> path;
         priority_queue<State<T>> openNodesPQ;                    // a priority queue of states to be evaluated
         openNodesPQ.push(searchable->getInitialState());
 
         set<State<T>> closedNodesSet;                             // a set of states already evaluated
 
-        while(!openNodesPQ.empty()) {
+        while (!openNodesPQ.empty()) {
             // remove the best node from openNodesPQ
             State<T> n = openNodesPQ.top();
             openNodesPQ.pop();
 
             closedNodesSet.insert(n); // so we won't check n again
 
-            if(searchable->isGoalState(n)) {
+            if (searchable->isGoalState(n)) {
                 path = backtrace(n);
-            }
-            vector<State<T>> * neighbors = searchable->getAllPossibleStates(n);
-            for(typename vector<State<T>>::iterator it = neighbors->begin() ; it != neighbors->end() ; it++) {
-
+            } else {
+                vector<State<T>> *neighbors = searchable->getAllPossibleStates(n);
+                for (typename vector<State<T>>::iterator it = neighbors->begin(); it != neighbors->end(); it++) {
+                }
             }
         }
         return path;
     };
-    vector<vector<T>*> backtrace(State<T>* goal) {
-        vector<vector<T>*> path;
+
+    vector<vector<State<T>> *> backtrace(State<T> *goal) {
+        vector<vector<T> *> path;
         path.push_back(goal);
         State<T> *parent = goal->getParent();
 
