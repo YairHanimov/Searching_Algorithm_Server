48cff4c4bb2390af3ba7ed79cdd1a4be1f72394a
eyalgolan <eyalgolan96@gmail.com>
2020-01-23 15:55:31 +0200

    working on bfs

--- BFS.h
+++ BFS.h
@@ -7,12 +7,39 @@
 
 #include "Searcher.h"
 #include <list>
+#include "queue"
+
 using namespace std;
 
 template<class T>
-class BFS : public Searcher<T>{
+class BFS : public Searcher<T> {
 public:
-   
+    explicit BFS(Searchable<T> *p) {
+
+    }
+
+    vector<State<T>> search(Matrix *problem) {
+        int evaluations = 0;
+        queue<State<T>*> bfsQueue;
+
+        State<T>* currentNode = problem->getInitialState();
+        bfsQueue.push(currentNode);
+        currentNode->setVisited();
+
+        while(!bfsQueue.empty()) {
+            evaluations++;
+            list<State<T>*> neighbors = problem->getAllPossibleStates(currentNode);
+            for (State<T> *currentNeighbor:neighbors) {
+                if(!currentNeighbor->getVisited()) {
+                    currentNeighbor->setVisited();
+                    currentNeighbor->setShortestPath(currentNode->getShortestPath()+currentNeighbor->getCost());
+                    currentNeighbor->setParent(currentNode);
+                    bfsQueue.pop();
+                }
+            }
+        }
+
+    }
 };
 
 
