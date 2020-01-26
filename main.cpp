ace1c24b9419b07ddb3fb7d2bdc37b482ea5608c
eyalgolan <eyalgolan96@gmail.com>
2020-01-26 18:03:09 +0200

    orginize search algo files

--- main.cpp
+++ main.cpp
@@ -8,20 +8,12 @@
 using namespace std;
 
 int main() {
-    cout << "blabla" << endl;
-//    auto *cm = new FileCacheManager<string,string>();
-//    auto *s = new StringReverser();
-//    auto *ch = new MyClientHandler<string,string>(cm,s);
-//    ch->handleClient(1,1);
-    //MySerialServer *serial_server = new MySerialServer(5600, ch);
-    //serial_server->open();
-    return 0;
 //    part two
-//    auto *cm = new FileCacheManager<string,string>();
-//    auto *s = new StringReverser();
-//    auto *ch = new MyTestClientHandler<string,string>(cm, s);
-//    MySerialServer *serial_server = new MySerialServer(5600, ch);
-//    serial_server->open();
+    auto *cm = new FileCacheManager<string,string>();
+    auto *s = new StringReverser();
+    auto *ch = new MyTestClientHandler<string,string>(cm, s);
+    MySerialServer *serial_server = new MySerialServer(5600, ch);
+    serial_server->open();
 
     //part three
 
@@ -32,11 +24,11 @@ int main() {
 //    vector<string> m = {line1,line2,line3,line4};
 //    vector<int> enter = {0,0};
 //    vector<int> exit = {3,3};
-
-    auto *cm = new FileCacheManager<string, string>();
-    auto *s = new StringReverser();
-    auto *ch = new MyClientHandler<string, string>(cm, s);
-    ch->handleClient(1, 1);
+//
+//    auto *cm = new FileCacheManager<string,string>();
+//    auto *s = new StringReverser();
+//    auto *ch = new MyClientHandler<string,string>(cm,s);
+//    ch->handleClient(1,1);
     //MySerialServer *serial_server = new MySerialServer(5600, ch);
     //serial_server->open();
     return 0;
